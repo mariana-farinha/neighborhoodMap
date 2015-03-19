a15bb5b71039407eae14a13938912253b6684446
Mary <mary.farinha@gmail.com>
2015-03-19 01:14:22 +0000

    Improve UI

--- scripts/app.js
+++ scripts/app.js
@@ -25,8 +25,16 @@ function AppViewModel() {
 	self.saved_value = ko.observable("");
 
 	self.markers = ko.observableArray([
+		new Marker(38.712088, -9.127635, "Fado"),
+		new Marker(38.752954, -9.184753, "Stadium of Light"),
+		new Marker(38.725917, -9.150767, "Sebastião José de Carvalho e Melo, 1st Marquis of Pombal"),
+		new Marker(38.787844, -9.390641, "Pena National Palace"),
+		new Marker(38.761489, -9.161807, "Estádio José Alvalade"),
+		new Marker(38.714072, -9.133497, "São Jorge Castle"),
+		new Marker(38.717080, -9.135792, "Martim Moniz"),
+		new Marker(38.715906, -9.124717, "Church of Santa Engrácia"),
 		new Marker(38.714063, -9.138957, "Rossio Square"),
-		new Marker(38.714072, -9.133497, "São Jorge Castle")
+		new Marker(38.708847, -9.136921, "Rua Augusta Arch")
 		]);
 
 	self.match = function(str1, str2,i){
@@ -59,7 +67,7 @@ function AppViewModel() {
 	var wikiURL = "http://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+ titles+"&exlimit=max";
     
     var wikiRequestTimeout = setTimeout(function() {
-    	$("#error").text("Failed to get info windows");
+    	$("#error").addClass("col-md-12 alert alert-danger").text("Failed to get info windows");
 
     }, 8000)
 
@@ -68,6 +76,10 @@ function AppViewModel() {
 		dataType: "jsonp",
 		success: function(response) {
 			clearTimeout(wikiRequestTimeout);
+			console.log(response);
+
+			$("#error").addClass("alert alert-success").text("Success!");
+
 			var i = 0;
 			for(var obj in response.query.pages) {
 
--- scripts/map.js
+++
@@ -1,9 +0,0 @@
-var mapOptions = {
-    center: { lat: 38.724143, lng: -9.139347}, 
-    zoom: 15
-};
-
-var map = new google.maps.Map(document.getElementById('map-canvas'),
-    mapOptions);
-
-    
